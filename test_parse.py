1e65b25114fb52bfea0ac81fce05fd27868e036c
Paul Mobbs <paul@Pauls-MacBook-Pro.local>
2018-10-30 14:54:36 -0700

    Initial commit

---
+++ test_parse.py
@@ -0,0 +1,10 @@
+import get_input_args
+#import argparse
+#parser = argparse.ArgumentParser(description='Argument parser for classify_images')
+#parser.add_argument('--dir', nargs='?', default='pet_images', help='Image Folder as --dir with default value \'pet_images\'')
+#parser.add_argument('--arch', nargs='?', default='vgg', help='CNN Model Architecture as --arch with default value \'vgg\'')
+#parser.add_argument('--dogfile', nargs='?', default='dognames.txt', help='Text File with Dog Names as --dogfile with default value \'dognames.txt\'')
+#
+#args = parser.parse_args()
+#print(args)
+get_input_args.get_input_args()
